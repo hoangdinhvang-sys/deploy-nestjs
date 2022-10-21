e9543901cb68ce5aabf8027928a16ffdc84467ca
hoangdinhvang-sys <chimgiay@gmail.com>
2022-10-21 10:19:27 +0700

    update 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from VangHD!';
+    return 'Hello World from VangHD, love you so much!';
   }
 }
